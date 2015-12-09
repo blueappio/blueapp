a06071cce4d28efccfd1eebbcf1c80e33acd4ac9
Kushi171987 <kushal@vensi.com>
2015-12-10 01:11:04 +0530

    Removed old individual node code, added js to get the swagger & gatt-ip.

---
+++ index.js
@@ -0,0 +1,9 @@
+(typeof process === 'object' && process + '' === '[object process]')) {
+	var blueappapi = require('swagger-client');
+	var blueappgattip = require('gatt-ip-js');
+
+	module.exports = {
+		api: blueappapi,
+		gattip: blueappgattip
+	};
+}
